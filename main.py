dd5679f653a7e8e7d1684c236683be7a45e8c174
SanjayKumar3110 <sanjaykumar785231@gmail.com>
2026-03-02 15:08:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    print("---------------------------")
+    print("         Welcome ")
+    print("---------------------------")
+    
+    main()
